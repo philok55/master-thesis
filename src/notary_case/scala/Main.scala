7e819d7be0de878eb1f0847323e0b02e8e3b221f
Philo Decroos <philo0508@hotmail.com>
2022-04-30 18:48:58 +0200

    Building Main file that does nothing

--- src/notary_case/scala/Main.scala
+++ src/notary_case/scala/Main.scala
@@ -4,18 +4,19 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorRefResolver
 import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl._
 
 object NotaryMain {
   sealed trait Scenarios { val resolver:ActorRefResolver }
   final case class TestCase1(resolver:ActorRefResolver) extends Scenarios
 
   def apply(): Behavior[Scenarios] =
-    Behaviors.setup { context =>
-      context.log.info("NotaryMain started")
-    }
+    Behaviors.setup { context => Behaviors.receive { (context, message) =>
+      Behaviors.stopped
+    }}
 }
 
 object FirstTestCase extends App {
-  val notaryMain: ActorSystem[BankingMain.Scenarios] = ActorSystem(NotaryMain(), "NotaryExample")
-  val resolver = ActorRefResolver(NotaryMain)
+  val notaryMain: ActorSystem[NotaryMain.Scenarios] = ActorSystem(NotaryMain(), "NotaryExample")
+  val resolver = ActorRefResolver(notaryMain)
 }
